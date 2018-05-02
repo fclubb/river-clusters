15cd219c5d2a3c6a1b28375bae58698a85c2ff00
fclubb <f.clubb25@gmail.com>
2018-05-02 10:07:30 +0200

    Adding function to plot all tribs

--- plot_river_clusters.py
+++ plot_river_clusters.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from matplotlib import rcParams
+from glob import glob
 
 def read_river_profile_csv(DataDirectory, fname_prefix):
     """
@@ -172,10 +173,54 @@ def PlotAllProfiles(DataDirectory, fname_prefix):
     plt.clf()
     #plt.show()
 
+def PlotProfilesAllTribuatires(DataDirectory,fname_prefix):
+    """
+    Function to make individual plots for each basin, with all the tributaries.
+    Coloured by source.
+
+    Args:
+        DataDirectory (str): the data directory
+        fname_prefix (str): the filename of the DEM without extension
+
+    Author: FJC
+    """
+    # Set up fonts for plots
+    label_size = 10
+    rcParams['font.family'] = 'sans-serif'
+    rcParams['font.sans-serif'] = ['arial']
+    rcParams['font.size'] = label_size
+
+    for fname in glob(DataDirectory+"*_all_tribs*.csv"):
+
+        # set up a figure
+        fig = plt.figure(1, facecolor='white',figsize=(4.92126,3.2))
+        gs = plt.GridSpec(100,100,bottom=0.15,left=0.1,right=0.9,top=0.9)
+        ax = fig.add_subplot(gs[5:100,10:95])
+
+        df = pd.read_csv(fname)
+
+        # get the profile IDs
+        source_ids = df['source_id'].unique()
+        basin_id = df['basin_id'][0]
+
+        for id in source_ids:
+            print ('This id is: ', id)
+            this_df = df[df['source_id'] == id]
+            print (this_df)
+            ax.plot(this_df['distance_from_outlet'], this_df['elevation'])
+
+        ax.set_xlabel('Distance from outlet (m)')
+        ax.set_ylabel('Elevation (m)')
+
+        plt.savefig(DataDirectory+fname_prefix+'_'+str(basin_id)+'_profiles.png', dpi=300)
+        plt.clf()
+
+
 if __name__ == '__main__':
 
     DataDirectory = '/home/clubb/Data_for_papers/river_spaghetti/Pozo/'
     fname_prefix = 'Pozo_DTM'
-    PlotAllProfiles(DataDirectory,fname_prefix)
-    PlotAllProfilesNormalisedElev(DataDirectory,fname_prefix)
-    PlotAllProfilesNormalised(DataDirectory,fname_prefix)
+    # PlotAllProfiles(DataDirectory,fname_prefix)
+    # PlotAllProfilesNormalisedElev(DataDirectory,fname_prefix)
+    # PlotAllProfilesNormalised(DataDirectory,fname_prefix)
+    PlotProfilesAllTribuatires(DataDirectory,fname_prefix)
