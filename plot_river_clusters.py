22bfbfd0a7824911decd1cf5cd3cf701e7bc1a61
fclubb <f.clubb25@gmail.com>
2018-05-03 12:02:20 +0200

    Adding functions to plot channels from all sources

--- plot_river_clusters.py
+++ plot_river_clusters.py
@@ -25,52 +25,38 @@ def read_river_profile_csv(DataDirectory, fname_prefix):
     return df
 
 #---------------------------------------------------------------------#
-# CLUSTERING FUNCTIONS
+# ANALYSIS FUNCTIONS
 #---------------------------------------------------------------------#
 
 def ClusterProfiles(df):
     """
     Cluster the profiles from the river profile dataframe
+    We will take the dataframe, and create another one in which each column
+    is a different profile.
 
     Args:
         df: pandas dataframe from the river profile csv.
 
     Author: FJC
     """
-
     # set up the array for clustering
-    source_ids = df['source_id'].unique()
-    n_profiles = len(source_ids)
-    cluster_matrix =
 
-    # # Do the clustering
-    # Z = hac.linkage(clu, method='single', metric='correlation')
-    #
-    # # Plot dendogram
-    # plt.figure(figsize=(25, 10))
-    # plt.title('Hierarchical Clustering Dendrogram')
-    # plt.xlabel('sample index')
-    # plt.ylabel('distance')
-    # hac.dendrogram(
-    #     Z,
-    #     leaf_rotation=90.,  # rotates the x axis labels
-    #     leaf_font_size=8.,  # font size for the x axis labels
-    # )
-    # plt.show()
 
-def ClusterAllTribuatryPlots(DataDirectory, fname_prefix, basin_id):
-    """
-    Cluster the profiles of all the tributaries in a basin.
 
-    Args:
-        DataDirectory: the data directory
-        fname_prefix: name of the DEM without extension
-        basin_id: the junction number of the basin you want to cluster
+    # Do the clustering
+    Z = hac.linkage(clusterDf, method='single', metric='correlation')
 
-    Author: FJC
-    """
-    df = pd.read_csv(DataDirectory+fname_prefix+'_all_tribs_'+str(basin_id)+'.csv')
-    ClusterProfiles(df)
+    # Plot dendogram
+    plt.figure(figsize=(25, 10))
+    plt.title('Hierarchical Clustering Dendrogram')
+    plt.xlabel('sample index')
+    plt.ylabel('distance')
+    hac.dendrogram(
+        Z,
+        leaf_rotation=90.,  # rotates the x axis labels
+        leaf_font_size=8.,  # font size for the x axis labels
+    )
+    plt.show()
 
 #---------------------------------------------------------------------#
 # PLOTTING FUNCTIONS
@@ -248,14 +234,97 @@ def PlotProfilesAllTribuatires(DataDirectory,fname_prefix):
         plt.savefig(DataDirectory+fname_prefix+'_'+str(basin_id)+'_profiles.png', dpi=300)
         plt.clf()
 
+def PlotProfilesAllSourcesSlope(DataDirectory,fname_prefix,slope_window_size=3):
+    """
+    Function to make a plot of all the channels coloured by source
+
+    Args:
+        DataDirectory (str): the data directory
+        fname_prefix (str): the filename of the DEM without extension
+        slope_window_size (int): number of points on the channel from which to calculate slope
+
+    Author: FJC
+    """
+    # Set up fonts for plots
+    label_size = 10
+    rcParams['font.family'] = 'sans-serif'
+    rcParams['font.sans-serif'] = ['arial']
+    rcParams['font.size'] = label_size
+
+    for fname in glob(DataDirectory+"*_all_sources*.csv"):
+
+        # set up a figure
+        fig = plt.figure(1, facecolor='white',figsize=(4.92126,3.2))
+        gs = plt.GridSpec(100,100,bottom=0.15,left=0.1,right=0.9,top=0.9)
+        ax = fig.add_subplot(gs[5:100,10:95])
+
+        df = pd.read_csv(fname)
+
+        # get the profile IDs
+        source_ids = df['source_id'].unique()
+
+        for id in source_ids:
+            print ('This id is: ', id)
+            this_df = df[df['source_id'] == id]
+            slope = this_df['slope'][this_df['slope'] != -9999]
+            dist =  this_df['distance_from_source'][this_df['slope'] != -9999]
+            ax.plot(dist,slope, lw=1)
+
+        ax.set_xlabel('Distance from source (m)')
+        ax.set_ylabel('Gradient')
+
+        plt.savefig(DataDirectory+fname_prefix+'_profiles_sources_slope.png', dpi=300)
+        plt.clf()
+
+def PlotProfilesAllSourcesElev(DataDirectory,fname_prefix,slope_window_size=3):
+    """
+    Function to make a plot of all the channels coloured by source
+
+    Args:
+        DataDirectory (str): the data directory
+        fname_prefix (str): the filename of the DEM without extension
+        slope_window_size (int): number of points on the channel from which to calculate slope
+
+    Author: FJC
+    """
+    # Set up fonts for plots
+    label_size = 10
+    rcParams['font.family'] = 'sans-serif'
+    rcParams['font.sans-serif'] = ['arial']
+    rcParams['font.size'] = label_size
+
+    for fname in glob(DataDirectory+"*_all_sources*.csv"):
+
+        # set up a figure
+        fig = plt.figure(1, facecolor='white',figsize=(4.92126,3.2))
+        gs = plt.GridSpec(100,100,bottom=0.15,left=0.1,right=0.9,top=0.9)
+        ax = fig.add_subplot(gs[5:100,10:95])
+
+        df = pd.read_csv(fname)
+
+        # get the profile IDs
+        source_ids = df['source_id'].unique()
+
+        for id in source_ids:
+            print ('This id is: ', id)
+            this_df = df[df['source_id'] == id]
+            source_elev = this_df['elevation'].max()
+            this_df['normalised_elev'] = this_df['elevation']/source_elev
+            ax.plot(this_df['distance_from_source'], this_df['normalised_elev'], lw=1)
+
+        ax.set_xlabel('Distance from source (m)')
+        ax.set_ylabel('$z/{z_{CH}}$')
+
+        plt.savefig(DataDirectory+fname_prefix+'_profiles_sources.png', dpi=300)
+        plt.clf()
 
 if __name__ == '__main__':
 
-    DataDirectory = '/home/clubb/Data_for_papers/river_spaghetti/Pozo/'
-    fname_prefix = 'Pozo_DTM'
-    basin_id = 641
+    DataDirectory = '/home/clubb/Data_for_papers/river_spaghetti/Pozo/cat1/'
+    fname_prefix = 'Pozo_cat1_UTM11_WGS84_1m'
     # PlotAllProfiles(DataDirectory,fname_prefix)
     # PlotAllProfilesNormalisedElev(DataDirectory,fname_prefix)
     # PlotAllProfilesNormalised(DataDirectory,fname_prefix)
     #PlotProfilesAllTribuatires(DataDirectory,fname_prefix)
-    ClusterAllTribuatryPlots(DataDirectory,fname_prefix,basin_id)
+    PlotProfilesAllSourcesSlope(DataDirectory,fname_prefix)
+    PlotProfilesAllSourcesElev(DataDirectory,fname_prefix)
